01b0b6875dff217d9fb8f9f9cf5befe66ec064f1
Tharindu Rangana <trangana21@gmail.com>
2025-03-07 19:12:51 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'VizzIt',
+  webDir: 'www'
+};
+
+export default config;
